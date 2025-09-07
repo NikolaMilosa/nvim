e58aa4d3d2ba06886ebe01b42e32a014f0c0c227
nikolamilosa <nikolamilosa20@gmail.com>
2025-09-08 00:30:41 +0200

    removing unneeded remaps

--- lua/nikola/remap.lua
+++ lua/nikola/remap.lua
@@ -25,44 +25,5 @@ set({ "n", "v" }, "ge", "G$0")
 set({ "n", "v" }, "gl", "$")
 set({ "n", "v" }, "gh", "0")
 
-local function open_file_in_split(direction)
-    -- grab the word (filename) under cursor
-    local file = vim.fn.expand("%")
-
-    -- check if file exists
-    if vim.fn.filereadable(file) == 0 then
-        vim.notify("File does not exist: " .. file, vim.log.levels.WARN)
-        return
-    end
-
-    -- decide split command
-    local cmd = ""
-    if direction == "left" then
-        cmd = "topleft vsplit"
-    elseif direction == "right" then
-        cmd = "botright vsplit"
-    elseif direction == "up" then
-        cmd = "topleft split"
-    elseif direction == "down" then
-        cmd = "botright split"
-    else
-        vim.notify("Invalid direction: " .. direction, vim.log.levels.ERROR)
-        return
-    end
-
-    -- open the file in the split
-    vim.cmd(cmd .. " " .. vim.fn.fnameescape(file))
-end
-
--- Keymaps
-set("n", "<leader>sh", function() open_file_in_split("left") end,
-    { desc = "Open file under cursor in left split" })
-set("n", "<leader>sl", function() open_file_in_split("right") end,
-    { desc = "Open file under cursor in right split" })
-set("n", "<leader>sk", function() open_file_in_split("up") end,
-    { desc = "Open file under cursor in up split" })
-set("n", "<leader>sj", function() open_file_in_split("down") end,
-    { desc = "Open file under cursor in down split" })
-
 set("n", "<M-j>", "<cmd>cnext<CR>")
 set("n", "<M-k>", "<cmd>cprev<CR>")
