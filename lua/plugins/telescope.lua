032f1b0ebb60101064ce881ee1f8851b1e72b6d8
nikolamilosa <nikolamilosa20@gmail.com>
2025-09-08 00:16:26 +0200

    help

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,8 +12,9 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<C-g>', builtin.live_grep, { noremap = true })
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>l', builtin.live_grep, { noremap = true })
+        vim.keymap.set('n', '<leader>hh', builtin.help_tags, { noremap = true })
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
