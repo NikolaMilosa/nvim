e909b695892e771c2d0fc399836697011b5ca27f
nikolamilosa <nikolamilosa20@gmail.com>
2025-09-07 23:10:48 +0200

    fixing poetry imports

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,26 @@
+-- Check if there is a `./poetry.lock` and source env if it exists
+local sourceVenvIfExists = function()
+    local match = vim.fn.glob(vim.fn.getcwd() .. "/poetry.lock")
+    -- Return if there are no matches
+    if match == "" then
+        return
+    end
+
+    vim.notify("Found poetry.lock in root", vim.log.levels.INFO)
+    local venv_path = vim.fn.trim(vim.fn.system("poetry env info --path"))
+    if vim.v.shell_error ~= 0 or venv_path == "" then
+        vim.notify("Could not get poetry venv path", vim.log.levels.ERROR)
+        return
+    end
+
+    vim.notify("Using Poetry venv: " .. venv_path, vim.log.levels.INFO)
+
+    -- Set Python for LSP / plugins
+    vim.g.python3_host_prog = venv_path .. "/bin/python"
+    vim.env.VIRTUAL_ENV = venv_path
+    vim.env.PATH = venv_path .. "/bin:" .. vim.env.PATH
+end
+
 return {
     {
         "neovim/nvim-lspconfig",
@@ -98,6 +121,12 @@ return {
                 }, {
                     { name = 'buffer' },
                 })
+            })
+
+            -- Try source on init
+            sourceVenvIfExists()
+            vim.api.nvim_create_autocmd({ "DirChanged" }, {
+                callback = sourceVenvIfExists
             })
         end,
     }
