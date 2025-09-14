390cf23ed1d953c00a38d26179947e188be69332
nikolamilosa <nikolamilosa20@gmail.com>
2025-09-14 20:54:36 +0200

    filtering out rust build directory from all file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,7 +29,7 @@ return {
             },
             pickers = {
                 find_files = {
-                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--no-ignore" },
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "--no-ignore", "--glob", "!target/*" },
                 }
             }
         })
