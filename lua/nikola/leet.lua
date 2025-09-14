4159d5925e13e4141225dc7e38975d1c205c983e
nikolamilosa <nikolamilosa20@gmail.com>
2025-09-14 20:48:35 +0200

    adding window to show cargo test

--- lua/nikola/leet.lua
+++ lua/nikola/leet.lua
@@ -3,9 +3,6 @@
 
 local M = {}
 
---- @todo: Map this to as shortcut
---- @todo: Add running of tests and printing their output in a window
-
 local open_buffer_and_set_as_active = function(name)
     vim.cmd("edit " .. name)
     vim.cmd("write!")
@@ -119,6 +116,71 @@ local yank_buffer = function()
     vim.fn.setreg("*", table.concat(lines_to_yank, "\n"))
 end
 
+local run_test_for_current_problem = function()
+    local filename = vim.fn.expand("%:t:r")
+
+    local ui = vim.api.nvim_list_uis()[1]
+    local width = math.floor(ui.width * 0.8)
+    local height = math.floor(ui.height * 0.8)
+    local col = math.floor((ui.width - width) / 2)
+    local row = math.floor((ui.height - height) / 2)
+
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = col,
+        row = row,
+        style = "minimal",
+        border = "rounded",
+    })
+
+    vim.api.nvim_set_current_buf(buf)
+
+    vim.fn.jobstart({ 'cargo', 'test', '--', filename }, {
+        term = true
+    })
+
+    vim.keymap.set("n", "q", function()
+        if vim.api.nvim_win_is_valid(win) then
+            vim.api.nvim_win_close(win, true)
+        end
+    end, { buffer = buf, nowait = true, silent = true })
+end
+
+local create_new_problem = function()
+    --   Create a scratch buffer
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_set_current_buf(buf)
+    vim.bo[buf].filetype = "rust"
+
+    -- Paste the provided text (from args) or a default snippet
+    local text = { "// Paste the initialization code from leetcode below this comment.",
+        "// The comments will be ignored and whatever is commented will be skipped in the actual snippet. ",
+        "// ",
+        "// Enter the problem name between ``.",
+        "// **NOTE**: the problem name should be a single word without `*.rs` at the end.",
+        "// Problem name: `some_problem`", "//", "" }
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, text)
+
+    -- Find the line and column to place the cursor
+    local target_line = 6
+    local col = text[target_line]:find("some_problem") - 1
+    -- Set cursor position in the current window
+    vim.api.nvim_win_set_cursor(0, { target_line, col })
+
+    -- Map <CR> in normal mode for this buffer only
+    vim.keymap.set("n", "<CR>", function()
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+        parse_lines(lines)
+        if vim.api.nvim_buf_is_valid(buf) then
+            vim.api.nvim_buf_delete(buf, { force = true })
+        end
+    end, { buffer = buf, nowait = true })
+end
+
 function M.setup()
     local working_dir = vim.fn.getcwd()
     local pattern = "work/Personal/some-more-leet"
@@ -131,39 +193,11 @@ function M.setup()
     vim.notify("Detected leet dir, loading some utilities...", vim.log.levels.INFO)
 
     -- Add a command to open a buffer, paste the text and close the buffer
-    vim.api.nvim_create_user_command("LeetPaste", function()
-        --   Create a scratch buffer
-        local buf = vim.api.nvim_create_buf(false, true)
-        vim.api.nvim_set_current_buf(buf)
-        vim.bo[buf].filetype = "rust"
-
-        -- Paste the provided text (from args) or a default snippet
-        local text = { "// Paste the initialization code from leetcode below this comment.",
-            "// The comments will be ignored and whatever is commented will be skipped in the actual snippet. ",
-            "// ",
-            "// Enter the problem name between ``.",
-            "// **NOTE**: the problem name should be a single word without `*.rs` at the end.",
-            "// Problem name: `some_problem`", "//", "" }
-        vim.api.nvim_buf_set_lines(buf, 0, -1, false, text)
-
-        -- Find the line and column to place the cursor
-        local target_line = 6
-        local col = text[target_line]:find("some_problem") - 1
-        -- Set cursor position in the current window
-        vim.api.nvim_win_set_cursor(0, { target_line, col })
-
-        -- Map <CR> in normal mode for this buffer only
-        vim.keymap.set("n", "<CR>", function()
-            local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-            parse_lines(lines)
-            if vim.api.nvim_buf_is_valid(buf) then
-                vim.api.nvim_buf_delete(buf, { force = true })
-            end
-        end, { buffer = buf, nowait = true })
-    end, {
+    vim.api.nvim_create_user_command("LeetPaste", create_new_problem, {
         nargs = "*",
         desc = "Open a scratch buffer, paste text, and close it",
     })
+    vim.keymap.set("n", "<leader>o", create_new_problem)
 
     -- Add a command to yank the solution to system clipboard
     vim.api.nvim_create_user_command("LeetYank", yank_buffer, {
@@ -172,6 +206,13 @@ function M.setup()
 
     vim.keymap.set({ "x", "o", "n" }, "yal", yank_buffer)
     vim.keymap.set({ "x", "o", "n" }, "yil", yank_buffer)
+
+    -- Add a command to run tests from the current file
+    vim.api.nvim_create_user_command("LeetTest", run_test_for_current_problem, {
+        desc = "Run tests for the current problem"
+    })
+
+    vim.keymap.set("n", "<leader>t", run_test_for_current_problem)
 end
 
 return M
